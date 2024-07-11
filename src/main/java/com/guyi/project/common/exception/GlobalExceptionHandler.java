4da4b0e075aea6d0f669e71ad8966a9f0d594a4a
guyi <1802366955@qq.com>
2024-07-11 14:51:13 +0800

    全局异常处理器

---
+++ src/main/java/com/guyi/project/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.guyi.project.common.exception;
+
+import com.guyi.project.common.response.BaseResponse;
+import com.guyi.project.common.response.ResultUtil;
+import com.guyi.project.common.response.ReturnCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author 孤诣
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtil.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtil.error(ReturnCode.SYSTEM_ERROR);
+    }
+}

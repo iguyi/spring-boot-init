f592b945cc2725324baab38817178bd9cb1f0686
guyi <1802366955@qq.com>
2024-07-11 17:01:28 +0800

    初始化用户请求控制器

---
+++ src/main/java/com/guyi/project/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.guyi.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User 请求控制器
+ *
+ * @author 孤诣
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}

1f66f7c83926c525466f7fe309872f74fcca241b
guyi <1802366955@qq.com>
2024-07-11 17:08:49 +0800

    新增用户注册功能

--- src/main/java/com/guyi/project/controller/UserController.java
+++ src/main/java/com/guyi/project/controller/UserController.java
@@ -1,7 +1,19 @@
 package com.guyi.project.controller;
 
+import com.guyi.project.common.response.BaseResponse;
+import com.guyi.project.common.response.ResultUtil;
+import com.guyi.project.model.dto.user.UserRegisterRequest;
+import com.guyi.project.service.UserService;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+import static com.guyi.project.common.response.ReturnCode.SYSTEM_ERROR;
 
 /**
  * User 请求控制器
@@ -11,4 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Resource
+    private UserService userService;
+
+    // region 登录相关
+
+    @PostMapping("/register")
+    @ApiOperation("用户注册")
+    public BaseResponse<Boolean> register(@RequestBody UserRegisterRequest registerRequest) {
+        Boolean result = userService.register(registerRequest);
+        if (result != null && result) {
+            return ResultUtil.ok(true);
+        }
+        return ResultUtil.error(SYSTEM_ERROR);
+    }
+
+    // endregion
 }

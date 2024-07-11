7e4d423bdd4547e40bcdc72a045eea0f505f2620
guyi <1802366955@qq.com>
2024-07-11 17:08:14 +0800

    新增用户注册方法

--- src/main/java/com/guyi/project/service/UserService.java
+++ src/main/java/com/guyi/project/service/UserService.java
@@ -1,5 +1,6 @@
 package com.guyi.project.service;
 
+import com.guyi.project.model.dto.user.UserRegisterRequest;
 import com.guyi.project.model.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -9,5 +10,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @author 孤诣
  */
 public interface UserService extends IService<User> {
-
+    /**
+     * 用户注册请求
+     *
+     * @param registerRequest 用户注册请求参数
+     * @return 如果返回值为 true, 则注册成功
+     */
+    Boolean register(UserRegisterRequest registerRequest);
 }

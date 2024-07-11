d6d0e7d32aa982903bbb326a1d79e46c03945042
guyi <1802366955@qq.com>
2024-07-11 14:14:16 +0800

    初始化 User 表的 Mapper 类

---
+++ src/main/java/com/guyi/project/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.guyi.project.mapper;
+
+import com.guyi.project.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 针对表【user(用户)】的数据库操作 Mapper
+ *
+ * @author 孤诣
+ * @see com.guyi.project.model.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+

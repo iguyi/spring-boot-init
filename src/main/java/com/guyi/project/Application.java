5f9b5ebdbd01ed857543f3d7d3bad6646579a0ff
guyi <1802366955@qq.com>
2024-07-11 16:46:18 +0800

    开启项目配置属性

--- src/main/java/com/guyi/project/Application.java
+++ src/main/java/com/guyi/project/Application.java
@@ -1,14 +1,17 @@
 package com.guyi.project;
 
+import com.guyi.project.common.constant.ProjectProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author 孤诣
  */
 @SpringBootApplication
 @MapperScan("com.guyi.project.mapper")
+@EnableConfigurationProperties(ProjectProperties.class)
 public class Application {
 
     public static void main(String[] args) {

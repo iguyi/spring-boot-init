470014e78118764edf9339be0677e706c9293eb3
guyi <1802366955@qq.com>
2024-07-12 15:39:48 +0800

    用户登录

--- src/main/java/com/guyi/project/service/impl/UserServiceImpl.java
+++ src/main/java/com/guyi/project/service/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.guyi.project.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.guyi.project.common.constant.ProjectProperties;
 import com.guyi.project.common.exception.BusinessException;
 import com.guyi.project.common.util.StringCheckUtil;
 import com.guyi.project.mapper.UserMapper;
+import com.guyi.project.model.dto.user.UserLoginRequest;
 import com.guyi.project.model.dto.user.UserRegisterRequest;
 import com.guyi.project.model.entity.User;
+import com.guyi.project.model.vo.user.UserVO;
 import com.guyi.project.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
@@ -14,9 +17,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
-import static com.guyi.project.common.constant.UserConstant.USER_ACCOUNT_RULE;
-import static com.guyi.project.common.constant.UserConstant.USER_PASSWORD_RULE;
+import static com.guyi.project.common.constant.UserConstant.*;
 import static com.guyi.project.common.response.ReturnCode.DB_SAVE_ERROR;
 import static com.guyi.project.common.response.ReturnCode.PARAMS_ERROR;
 
@@ -60,6 +63,39 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             return true;
         }
         throw new BusinessException(DB_SAVE_ERROR);
+    }
+
+    @Override
+    public UserVO login(UserLoginRequest loginRequest) {
+        // 参数校验
+        if (loginRequest == null) {
+            throw new BusinessException(PARAMS_ERROR);
+        }
+        // 账号、密码校验
+        String userAccount = loginRequest.getUserAccount();
+        if (StringUtils.isBlank(userAccount) || StringCheckUtil.notIsRegex(userAccount, USER_ACCOUNT_RULE)) {
+            throw new BusinessException(PARAMS_ERROR, "账号格式有误");
+        }
+        // 密码校验
+        String userPassword = loginRequest.getUserPassword();
+        if (StringUtils.isBlank(userPassword) || StringCheckUtil.notIsRegex(userPassword, USER_PASSWORD_RULE)) {
+            throw new BusinessException(PARAMS_ERROR, "密码格式错误");
+        }
+
+        // 查看用户信息是否正确
+        QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        userQueryWrapper.eq("userAccount", userAccount)
+                .eq("userPassword", encryptUserPassword(userPassword));
+        User user = this.getOne(userQueryWrapper);
+        if (user == null) {
+            throw new BusinessException(PARAMS_ERROR, "账号或密码错误");
+        }
+
+        // 用户信息正确, 返回用户登录态
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(user, userVO);
+        userVO.setId(user.getId());
+        return userVO;
     }
 
     /**
